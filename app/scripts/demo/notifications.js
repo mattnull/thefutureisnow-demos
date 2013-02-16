2503b2f5b5ac355f6696867920968c70e4ab1cfd
Matt <hello@mattnull.com>
2013-02-16 14:30:27 -0700

    updating

--- app/scripts/demo/notifications.js
+++ app/scripts/demo/notifications.js
@@ -4,10 +4,10 @@
  */
  
 var NotificationsDemo = (function(){
-	$('#allow-notifications').on('click', function() {
+	$('#allow-notifications').on('click', function(){
 	    // if we do not have permission, ask for it
 	    // 0 means we have permissions (I know its weird)
-	    if (window.webkitNotifications.checkPermission() !== 0) {
+	    if(window.webkitNotifications.checkPermission() !== 0){
 	        window.webkitNotifications.requestPermission();
 	    }
 	    else{
